d50f24760969d9508849234dcec49444444640d2
Adriano Rosa <info@adrianorosa.com>
2025-10-26 23:22:48 -0300

    Add main entry point for token-gen module
    Introduces index.mjs to re-export formatterStyledComponentTypes, formatterEs6File, customHeader, and filterCustom from the src directory for easier module access.

---
+++ token-gen/index.mjs
@@ -0,0 +1,11 @@
+import formatterStyledComponentTypes from './src/formatterStyledComponentTypes.mjs';
+import formatterEs6File from './src/formatterEs6File.mjs';
+import customHeader from './src/defaultHeader.mjs';
+import filterCustom from './src/filterCustom.mjs';
+
+export {
+  formatterStyledComponentTypes,
+  formatterEs6File,
+  customHeader,
+  filterCustom
+}
